7ac00f855c4d575605ccbc25416a1da60f29dbc4
Evgeniy <e.bashtan@qiwi.com>
2020-11-02 22:26:35 +0300

    Clear project

---
+++ src/components/content/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Button = styled.button`
+  border-radius: 50px;
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`
+
+export const Content = ()=>{
+    return <div>content</div>
+}
