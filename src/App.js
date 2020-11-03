0bef2bc0d56121d2ced16ad2587fabe3f5e0abb6
Evgeniy <e.bashtan@qiwi.com>
2020-11-03 22:46:27 +0300

    HEADER

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import {Content} from './components/content';
-import {Header} from './components/header';
+import {Header as HeaderBase} from './components/header';
 import {Footer} from './components/footer';
+import styled from 'styled-components';
+
+const IntroSection = styled.section`
+  padding-top: 200px;
+  position: relative;
+  background-size: cover;
+  z-index: 1;
+  background-image: url(assets/background-1920x1080.jpg);
+
+  &::before{
+    content: '';
+    z-index: -1;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: rgba(0,0,0, .2);
+  }
+`
+
+const Header = styled(HeaderBase)`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  right: 0;
+  z-index: 100;
+`
 
 export const App = () => {
   return (
     <>
-      <Header/>
+      <IntroSection>
+        <Header/>
+      </IntroSection>
       <Content/ >
       <Footer />
     </>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 
-export const Header = ()=>{
-    return <div>header</div>
+const HeaderContainer = styled.header`
+    @media (min-width: 992px) {
+        width: 960px;
+        max-width: 100%;
+    }
+`
+
+
+const Content = styled.div`
+
+`
+
+
+const Wrapper = styled.div`
+    border-radius: 5px;
+    padding: 30px 40px 20px;
+    background: #FFAD01;
+    color: #fff;
+`;
+
+export const Header = ({className})=>{
+    return <Wrapper className={className}>header</Wrapper>
 }
